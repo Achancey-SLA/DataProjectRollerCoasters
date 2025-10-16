f21aea305dbb9737424bf29bdd7e3efa238f49db
AlexChancey <achancey27@scienceleadership.org>
2025-10-16 09:21:03 -0400

    added goodcoaster

---
+++ src/GoodCoaster.java
@@ -0,0 +1,48 @@
+public class GoodCoaster extends RollerCoaster {
+    float rating;
+    String country;
+    String manufacturer;
+    int duels;
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getDuels() {
+        return duels;
+    }
+
+    public void setDuels(int duels) {
+        this.duels = duels;
+    }
+
+    public GoodCoaster(String name, String park, int rank, float rating, String country, String manufacturer, int duels) {
+        super(name, park, rank);
+        this.rating = rating;
+        this.country = country;
+        this.manufacturer = manufacturer;
+        this.duels = duels;
+
+
+    }
+}
