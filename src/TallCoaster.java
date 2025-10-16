82f9656da155059e70993ca1dea054d0d5d0ab81
MinghaoZhang <mzhang26@scienceleadership.org>
2025-10-16 09:20:36 -0400

    V1

--- src/TallCoaster.java
+++ src/TallCoaster.java
@@ -1,3 +1,44 @@
-public class TallCoaster extends RollerCoaster {
+import java.time.LocalDate;
 
+public class TallCoaster extends RollerCoaster {
+    boolean operating;
+    LocalDate opened;
+    Float height;
+
+    public boolean isOperating() {
+        return operating;
+    }
+
+    public void setOperating(boolean operating) {
+        this.operating = operating;
+    }
+
+    public Float getHeight() {
+        return height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+    public LocalDate getOpened() {
+        return opened;
+    }
+
+    public void setOpened(LocalDate opened) {
+        this.opened = opened;
+    }
+
+    public TallCoaster(String name, String park, int rank, boolean operating, LocalDate opened, Float height) {
+        super(name, park, rank);
+        this.operating = operating;
+        this.opened = opened;
+        this.height = height;
+
+//public String toString() {
+
+        }
+        }
+
+    }
 }
